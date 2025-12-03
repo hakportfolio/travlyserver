8b54c3321a8dac56bae19533f319b87f32f695df
hakchulk <hakchulk@gmail.com>
2025-12-03 15:08:55 +0900

    data.sql#2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,17 @@
--- travly : insert categories and items
-
-DO
-$$
-DECLARE
-    row_count integer;
-BEGIN
-    -- 1. 특정 테이블의 row 개수를 조회합니다.
-    SELECT COUNT(*) INTO row_count FROM filter_category;
-
-    -- 2. row 개수가 0보다 크면 (즉, 데이터가 이미 있으면) 함수를 종료합니다.
-    IF row_count > 0 THEN
-        RAISE NOTICE 'Table filter_category already has % rows. Skipping initialization.', row_count;
-        RETURN; -- 이 시점에서 DO 블록을 종료하고 아래 SQL을 실행하지 않습니다.
-    END IF;
-    
-	-- Category: 1. 누구와 떠나나요? (최대 3개)
+-- Category: 1. 누구와 떠나나요? (최대 3개)
 	INSERT INTO filter_category (id, name, multi_select, order_num, created_at)
-	VALUES (1, '누구와 떠나나요?(최대3개)', TRUE, 1, NOW());
+	VALUES (1, '누구와 떠나나요?(최대3개)', TRUE, 1, NOW())
+	ON CONFLICT (id) DO NOTHING;
 	
 	-- Category: 2. 여행 기간? (1개)
 	INSERT INTO filter_category (id, name, multi_select, order_num, created_at)
-	VALUES (2, '여행 기간?(1개)', FALSE, 2, NOW());
+	VALUES (2, '여행 기간?(1개)', FALSE, 2, NOW())
+	ON CONFLICT (id) DO NOTHING;
 	
 	-- Category: 3. 여행 스타일? (최대 5개)
 	INSERT INTO filter_category (id, name, multi_select, order_num, created_at)
-	VALUES (3, '여행 스타일?(최대5개)', TRUE, 3, NOW());
+	VALUES (3, '여행 스타일?(최대5개)', TRUE, 3, NOW())
+	ON CONFLICT (id) DO NOTHING;
 	
 	-- Item: 카테고리 1 (누구와 떠나나요?)
 	INSERT INTO filter_item (id, filter_category_id, name, order_num, created_at) VALUES
@@ -34,7 +21,8 @@ BEGIN
 	(4, 1, '배우자와', 4, NOW()),
 	(5, 1, '아이와', 5, NOW()),
 	(6, 1, '부모님과', 6, NOW()),
-	(7, 1, '반려동물과', 7, NOW());
+	(7, 1, '반려동물과', 7, NOW())
+	ON CONFLICT (id) DO NOTHING;
 	
 	-- Item: 카테고리 2 (여행 기간?)
 	INSERT INTO filter_item (id, filter_category_id, name, order_num, created_at) VALUES
@@ -44,7 +32,8 @@ BEGIN
 	(11, 2, '3박4일', 4, NOW()),
 	(12, 2, '4박5일', 5, NOW()),
 	(13, 2, '5박6일', 6, NOW()),
-	(14, 2, '7일 이상', 7, NOW());
+	(14, 2, '7일 이상', 7, NOW())
+	ON CONFLICT (id) DO NOTHING;
 	
 	-- Item: 카테고리 3 (여행 스타일?)
 	INSERT INTO filter_item (id, filter_category_id, name, order_num, created_at) VALUES
@@ -55,7 +44,8 @@ BEGIN
 	(19, 3, '여유롭게', 5, NOW()),
 	(20, 3, '문화/예술/역사', 6, NOW()),
 	(21, 3, '여행지 느낌', 7, NOW()),
-	(22, 3, '쇼핑/음식', 8, NOW());
+	(22, 3, '쇼핑/음식', 8, NOW())
+	ON CONFLICT (id) DO NOTHING;
 
 	-- badge
 	INSERT INTO badge (id, name, like_min, like_max) VALUES
@@ -63,7 +53,5 @@ BEGIN
 	(2, '입문자', 50, 199),
 	(3, '콜렉터', 200, 499),
 	(4, '전문가', 500, 999),
-	(5, '달인', 1000, 99999999);
-
-END
-$$;
+	(5, '달인', 1000, 99999999)
+	ON CONFLICT (id) DO NOTHING;
